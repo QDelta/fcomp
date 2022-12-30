c5fb6b50baa950f2a99ebea8151bf99fb8183bc4
qdelta <qdelta@qdelta.com>
2022-12-30 19:21:12 +0800

    inline ifte to improve performance

--- app/Prim/GM.hs
+++ app/Prim/GM.hs
@@ -40,10 +40,10 @@ compilePrimFn arity (name, inst) =
   (primNameMap ! name, arity,
     concat (replicate arity [Push (arity - 1), Eval]) ++ [inst, Update arity, Pop arity])
 
+-- and x y = case x of { False -> False; True -> y };
+--  or x y = case x of { True -> True; False -> y };
 compiledPrimFns :: [(Name, Int, Code)]
 compiledPrimFns =
-  -- and x y = case x of { False -> False; True -> y };
-  -- or x y = case x of { True -> True; False -> y };
   [ (primNameMap ! "and", 2, [Push 0, Eval, CaseJ [(0, [Pop 1, Pack 0 0]), (1, [Pop 1, Push 1, Eval])], Update 2, Pop 2])
   , (primNameMap ! "or",  2, [Push 0, Eval, CaseJ [(0, [Pop 1, Push 1, Eval]), (1, [Pop 1, Pack 1 0])], Update 2, Pop 2])
   ]
